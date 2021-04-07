a05260c6dfc3bde2aa8381fc800b4669d8881b25
Wellington Oliva <well.oliva@hotmail.com>
2021-04-07 20:20:01 -0300

    View and delete films

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -22,4 +22,12 @@ export class FilmesService {
     const ConfigParams = this.configService.configurarParametros(config);
     return this.http.get<Filme[]>(url, {params: ConfigParams});
   }
+
+  visualizar(id: number): Observable<Filme> {
+    return this.http.get<Filme>(url + id);
+  }
+
+  excluir(id: number): Observable<void> {
+    return this.http.delete<void>(url + id);
+  }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { debounceTime } from 'rxjs/operators';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { ConfigParams } from 'src/app/shared/models/config-params';
@@ -24,7 +25,8 @@ export class ListagemFilmesComponent implements OnInit {
   generos: Array<string>;
 
   constructor(private FilmesService: FilmesService,
-              private fb: FormBuilder) { }
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.filtrosListagem = this.fb.group({
@@ -52,6 +54,10 @@ export class ListagemFilmesComponent implements OnInit {
 
   onScroll(): void {
     this.listarFilmes();
+  }
+
+  abrir(id: number): void {
+    this.router.navigateByUrl('/filmes/' + id);
   }
 
   private listarFilmes(): void {
